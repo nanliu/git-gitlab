14536577d3535dbf613b488cd13103b57aab5157
Nan Liu <nan.liu@gmail.com>
2014-11-12 11:25:02 -0800

    Cleanup whitespace.

--- lib/git/gitlab/apiclient.rb
+++ lib/git/gitlab/apiclient.rb
@@ -5,59 +5,58 @@ require "grit"
 require "uri"
 
 module GitlabApi
-	class ApiClient
-		API_VERSION = "v3"
-		USER_AGENT = "nyonyonyonyonyo! Gitlab nyo!!"
-		attr_reader :client, :repository
+  class ApiClient
+    API_VERSION = "v3"
+    USER_AGENT = "nyonyonyonyonyo! Gitlab nyo!!"
+    attr_reader :client, :repository
 
-		def initialize
-			@repository = Grit::Repo.new(`git rev-parse --show-toplevel`.chomp)
-			config = @repository.config
+    def initialize
+      @repository = Grit::Repo.new(`git rev-parse --show-toplevel`.chomp)
+      config = @repository.config
 
-			url = config["gitlab.url"]
-			if url == nil
-				raise "Plase set 'git config gitlab.url ${Gitlab URL}'"
-			end
+      url = config["gitlab.url"]
+      if url == nil
+        raise "Plase set 'git config gitlab.url ${Gitlab URL}'"
+      end
 
-			token = config["gitlab.token"]
-			if token == nil
-				raise "Please set 'git config gitlab.token ${Gitlab Token}'"
-			end
+      token = config["gitlab.token"]
+      if token == nil
+        raise "Please set 'git config gitlab.token ${Gitlab Token}'"
+      end
 
-			Gitlab.configure do |config|
-			  config.endpoint       = "#{url}/api/#{API_VERSION}"
-			  config.private_token  = token
-			  config.user_agent     = USER_AGENT
-			end
+      Gitlab.configure do |config|
+        config.endpoint      = "#{url}/api/#{API_VERSION}"
+        config.private_token = token
+        config.user_agent    = USER_AGENT
+      end
 
-			@client = Gitlab.client
-		end
+      @client = Gitlab.client
+    end
 
-		def remote
-			if @repository.config.keys.include? "gitlab.remote"
-				@repository.config["gitlab.remote"]
-			else
-				"origin"
-			end
-		end
+    def remote
+      if @repository.config.keys.include? "gitlab.remote"
+        @repository.config["gitlab.remote"]
+      else
+        "origin"
+      end
+    end
 
-		def project_id
-			pid = if @repository.config.keys.include? "gitlab.projectid"
-					@repository.config["gitlab.projectid"].to_i
-				else
-					@repository.config["gitlab.project"]
-				end
+    def project_id
+      pid = if @repository.config.keys.include? "gitlab.projectid"
+              @repository.config["gitlab.projectid"].to_i
+            else
+              @repository.config["gitlab.project"]
+            end
 
-			if pid == nil
-				raise "Please set 'git config gitlab.projectid ${Gitlab Project id}' of git config gitlab.project ${NAMESPACE/PROJECT}"
-			end
+      if pid == nil
+        raise "Please set 'git config gitlab.projectid ${Gitlab Project id}' of git config gitlab.project ${NAMESPACE/PROJECT}"
+      end
 
-			begin
-				@client.project( URI.encode_www_form_component(pid.to_s)).id
-			rescue Gitlab::Error::NotFound => e
-				raise GitlabApi::Error::ProjectIdNotFound, "Can not find #{pid} Project."
-			end
-
-		end
-	end
+      begin
+        @client.project( URI.encode_www_form_component(pid.to_s)).id
+      rescue Gitlab::Error::NotFound => e
+        raise GitlabApi::Error::ProjectIdNotFound, "Can not find #{pid} Project."
+      end
+    end
+  end
 end
--- spec/mergerequest_spec.rb
+++ spec/mergerequest_spec.rb
@@ -2,17 +2,17 @@ require File.dirname(__FILE__) + '/spec_helper'
 require "git/gitlab"
 
 describe GitlabKernel, "create new mergerequest" do
-	
-	before do
-		@gitlab = GitlabKernel.new
 
-		@test_config = {:projectid => 8,
-			   :project_path => "numa08/git-gitlab",
-			   :gitlab_url => "http://vagrant-ubuntu-precise-64/"}
-	end
+  before do
+    @gitlab = GitlabKernel.new
 
-	it "should return mergerquest url" do
-		actual = @gitlab.create_merge_request("test", nil, "tem", "master")
-		expect(actual).to include("#{@test_config[:gitlab_url]}#{@test_config[:project_path]}/merge_requests")
-	end	
+    @test_config = {:projectid => 8,
+                    :project_path => "numa08/git-gitlab",
+                    :gitlab_url => "http://vagrant-ubuntu-precise-64/"}
+  end
+
+  it "should return mergerquest url" do
+    actual = @gitlab.create_merge_request("test", nil, "tem", "master")
+    expect(actual).to include("#{@test_config[:gitlab_url]}#{@test_config[:project_path]}/merge_requests")
+  end	
 end
